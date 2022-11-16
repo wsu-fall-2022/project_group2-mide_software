5ee1ec18d21891436fee16f8e28f88c8bd7d1cbf
Catguythe3rd <mathieuandlebaron@gmail.com>
2022-11-16 10:04:15 -0700

    Mathieu Lebaron
    "did you get it?"

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -4,7 +4,7 @@ require.context('../images/', true, /\.(gif|jpg|png|svg|eot|ttf|woff|woff2)$/i)
 require.context('../stylesheets/', true, /\.(css|scss)$/i)
 
 // First: Set up your name
-let std_name = "Your name goes here"
+let std_name = "Mathieu Lebaron and Alex"
 document.querySelector('#std_name').innerHTML = `<strong>${std_name}</strong>`
 
 //Then: comes everything else
