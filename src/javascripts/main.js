df9234d04f641ee4b446db17d59224b977f800e3
Alex <112414349+HandsomeDevil99@users.noreply.github.com>
2022-11-16 10:06:07 -0700

    my name
    my last name

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -4,8 +4,9 @@ require.context('../images/', true, /\.(gif|jpg|png|svg|eot|ttf|woff|woff2)$/i)
 require.context('../stylesheets/', true, /\.(css|scss)$/i)
 
 // First: Set up your name
-let std_name = "Mathieu Lebaron and Alex"
+let std_name = "Mathieu Lebaron and Alex Christensen"
 document.querySelector('#std_name').innerHTML = `<strong>${std_name}</strong>`
+
 
 //Then: comes everything else
 // TODO
