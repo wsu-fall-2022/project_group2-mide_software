540070530855326a0f430a8d457d83ba898d806d
Catguythe3rd <mathieuandlebaron@gmail.com>
2022-12-03 16:49:38 -0700

    Animation code no work
    There's some code that should move the BFG ball with some animation imports, but It needs to update each frame and I don't know how to do that since its an instantiated object.

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -4,7 +4,7 @@ import {PointerLockControls} from "three/examples/jsm/controls/PointerLockContro
 import {OBJLoader} from 'three/examples/jsm/loaders/OBJLoader.js';
 import {MTLLoader} from 'three/examples/jsm/loaders/MTLLoader.js';
 import {BallProjectile} from "./projectile";
-import {assertObject} from "@babel/core/lib/config/validation/option-assertions";
+import {AnimationClip, AnimationMixer, NumberKeyframeTrack, VectorKeyframeTrack} from "three";
 
 // Required by Webpack - do not touch
 require.context('../', true, /\.(html|json|txt|dat)$/i)
@@ -25,8 +25,9 @@ let camera = new THREE.PerspectiveCamera(45, canvas.clientWidth / canvas.clientW
 
 //player
 let player;
+let updatables = []
 //list of things that player can collide with
-var collidableMeshList = [];
+let collidableMeshList = [];
 
 renderer.setSize(canvas.clientWidth, canvas.clientHeight)
 renderer.setClearColor(0xFFEEEE)
@@ -38,7 +39,9 @@ let moveForward = false;
 let moveBackward = false;
 let moveLeft = false;
 let moveRight = false;
-let playerSpeed = 5;
+let playerDefaultSpeed = 4;
+let playerRunSpeed = 6.5;
+let playerSpeed = playerDefaultSpeed;
 let selectedWeapon = 2;
 let gunMesh;
 
@@ -96,6 +99,10 @@ playerControls.onKeyDown = function ( event ) {
 
             console.log(selectedWeapon)
             break;
+
+        case 'ShiftLeft':
+            playerSpeed = playerRunSpeed;
+            break;
     }
 }
 playerControls.onKeyUp = function ( event ) {
@@ -122,6 +129,10 @@ playerControls.onKeyUp = function ( event ) {
             moveRight = false;
             break;
 
+        case 'ShiftLeft':
+            playerSpeed = playerDefaultSpeed;
+            break;
+
     }
 }
 function onWindowResize() {
@@ -140,11 +151,12 @@ window.addEventListener( 'click', function () {
     }
 
 } );
-
 canvas.addEventListener( 'click', function () {
     playerControls.lock();
 
 } );
+
+//other listeners
 window.addEventListener( 'keydown', playerControls.onKeyDown );
 window.addEventListener( 'keyup', playerControls.onKeyUp );
 window.addEventListener( 'resize', onWindowResize );
@@ -164,16 +176,16 @@ export let textures = {
     shotgun: texLoader.load('./images/Shotgun Barrel Texture.jpg', function () {
         renderer.render(scene, camera)
     }),
-    BFGball1: texLoader.load('./images/Lightning Texture.png', function (texture) {
+    BFGball1: texLoader.load('./images/Lightning Texture.png', function () {
         renderer.render(scene, camera)
     }),
-    BFGball2: texLoader.load('./images/Electricity texture 4.png', function (texture) {
+    BFGball2: texLoader.load('./images/Electricity texture 4.png', function () {
         renderer.render(scene, camera)
     }),
     BFGgun: texLoader.load('./images/BFG Texture.jpg', function (texture) {
         texture.wrapS = THREE.RepeatWrapping
         texture.wrapT = THREE.RepeatWrapping
-        texture.repeat.set(6, 3)
+        texture.repeat.set(7, 3.15)
         renderer.render(scene, camera)
     })
 }
@@ -225,35 +237,46 @@ function loadGun()
     }
     else if (selectedWeapon === 2){
         // BFG 9000
-        geometry = new THREE.CylinderGeometry(1, 1,40, 30, 30)
+        geometry = new THREE.CylinderGeometry(1.5, 0.1,40, 30, 30)
         material = new THREE.MeshPhongMaterial({color: 0x999999})
         gunMesh = new THREE.Mesh(geometry, material)
-        gunMesh.translateY(-2)
+        gunMesh.translateY(-2.5)
         gunMesh.translateZ(5)
         gunMesh.rotateX(-Math.PI/1.9)
         gunMesh.rotateY(Math.PI)
         gunMesh.material.map = textures['BFGgun']
         camera.add(gunMesh)
-
-        //point in space where the projectile spawns in
-        geometry = new THREE.SphereGeometry(1, 1,1)
-        material = new THREE.MeshPhongMaterial({color: 0x999999})
-        let pointMesh = new THREE.Mesh(geometry, material)
-        scene.add(pointMesh)
     }}
 loadGun()
 
 function fireGun() {
     if (selectedWeapon === 1) {
-
+        console.log('Wheres the demon shattering Kaboom?')
     }
     else if (selectedWeapon === 2) {
         let ballProjectile = new BallProjectile()
-        let relativePositionVector = new THREE.Vector3(0, -10, -50).applyQuaternion(camera.quaternion)
+
+        //spawns the ball in front of the camera
+        let relativePositionVector = new THREE.Vector3(0, -15, -75).applyQuaternion(camera.quaternion)
         ballProjectile.BFGballMesh.quaternion.copy(camera.quaternion)
         ballProjectile.BFGballMesh.position.copy(camera.position).add(relativePositionVector)
+
+        //moves the BFGball forward using a bunch of animation libraries.
+        let ballEndPointVector = new THREE.Vector3(0, 0, -2000).applyQuaternion(ballProjectile.BFGballMesh.quaternion)
+        let times = [0, 1]
+        let positions = [ballProjectile.BFGballMesh.position, ballEndPointVector]
+        let positionKeyFrame = new VectorKeyframeTrack(".position", times, positions)
+        let positionAnimation = new AnimationClip("positionAnimation", 100, [positionKeyFrame])
+
+        let BFGmixer = new AnimationMixer(ballProjectile.BFGballMesh)
+        let action = BFGmixer.clipAction(positionAnimation);
+        action.play()
+
+        ballProjectile.BFGballMesh.tick = (delta) => BFGmixer.update(delta)
+        updatables.push(ballProjectile.BFGballMesh)
+
+        console.log('Vzoom!')
     }
-    console.log('Bang!')
 }
 
 //Level Model
@@ -361,16 +384,16 @@ function animate() {
     } lights()
 
     if (moveForward === true){
-        playerControls.moveForward(  playerSpeed)
+        playerControls.moveForward(playerSpeed)
     }
     if (moveBackward === true){
-        playerControls.moveForward(  -playerSpeed)
+        playerControls.moveForward(-playerSpeed)
     }
     if (moveLeft === true){
-        playerControls.moveRight(  -playerSpeed)
+        playerControls.moveRight(-playerSpeed/2)
     }
     if (moveRight === true){
-        playerControls.moveRight(  playerSpeed)
+        playerControls.moveRight(playerSpeed/2)
     }
 
     player.position.set(camera.position.x,camera.position.y,camera.position.z)
